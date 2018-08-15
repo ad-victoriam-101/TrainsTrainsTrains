b5a5fc4352eaf3c215c26ef1163e3274fdb8a614
Ad_Victoriam <keeper24@gmail.com>
2018-08-14 21:06:01 -0700

    made firebase

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,12 @@
-console.log("test")
+var config = {
+    apiKey: "AIzaSyBu5bwXKKYb6OpecafeGkE9udKDW2y26c4",
+    authDomain: "fntrainsman.firebaseapp.com",
+    databaseURL: "https://fntrainsman.firebaseio.com",
+    projectId: "fntrainsman",
+    storageBucket: "fntrainsman.appspot.com",
+    messagingSenderId: "765289580670"
+  };
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
