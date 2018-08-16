110aeec5b1de7d51acddae4dcd1e418cf34c767d
ad-victoriam-101 <keeper24@gmail.com>
2018-08-16 09:56:51 -0700

    added a clock.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -54,4 +54,11 @@ var config = {
       console.log("Errors Handled: " + errorObject.code);
   });
 //   moment BS
-
+function displayTime() {
+  var time = moment().format('HH:mm:ss');
+  $('#clock').html(time);
+  setTimeout(displayTime, 1000);
+}
+$(document).ready(function() {
+  displayTime();
+});
